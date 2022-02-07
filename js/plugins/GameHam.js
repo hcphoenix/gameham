7fcabd95f13fac108c49df816efacee4a7f62f34
content-deleted <contentdeleted@gmail.com>
2022-02-07 11:43:38 -0500

    fixed bug with state displaying on player characters

--- js/plugins/GameHam.js
+++ js/plugins/GameHam.js
@@ -58,7 +58,7 @@ var GameHam = GameHam || {};
   GameHam.cycleStateIcon = function(partyMemberId) {
     //var s = $gameParty.members()[partyMemberId].states(); //priority
     //var highest = s.sort((a, b)=> a.priority - b.priority); 
-    if(!$gameParty.members()[1].states().length) return "Icons_0";
+    if(!$gameParty.members()[partyMemberId].states().length) return "Icons_0";
     var icon = $gameParty.members()[partyMemberId].states().sort((b, a)=> a.priority - b.priority)[0].iconIndex + 1;
     return "Icons_" + Math.min(icon, 200);
   } // $gameParty.members()[1].states().length
