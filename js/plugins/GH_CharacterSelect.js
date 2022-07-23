1ab3f3984421aae38095d8fbee4b2eb9779c84e3
content-deleted <contentdeleted@gmail.com>
2022-07-23 03:34:46 -0400

    Character select most of the way there

--- js/plugins/GH_CharacterSelect.js
+++ js/plugins/GH_CharacterSelect.js
@@ -111,15 +111,23 @@ Scene_CharacterSelect.prototype.initialize = function() {
     this.startFadeIn(60, false);
 
     // iidk how to play the music we want BattleManager.playBattleBgm();
+    this.timer = 0;
 }
 
 Scene_CharacterSelect.prototype.load_images = function() {
-    this._portbig_img = ImageManager.loadCharacterSelectBitmap("select_portbig_a");
-    this._portsmall_img = ImageManager.loadCharacterSelectBitmap("select_portsmall_leftside_a");
+    this._portbig_img = ImageManager.loadCharacterSelectBitmap("select_portbig");
+    this._portsmall_img = ImageManager.loadCharacterSelectBitmap("select_portsmall");
     this._BG_img = ImageManager.loadCharacterSelectBitmap("select_BG"); 
     this._shadow_img = ImageManager.loadCharacterSelectBitmap("select_minimap_shadow");
-    this._support_img = ImageManager.loadCharacterSelectBitmap("select_portbig_leftsupport");
-    this._title_img = ImageManager.loadCharacterSelectBitmap("select_title_a");
+    this._support_img = ImageManager.loadCharacterSelectBitmap("select_portbig_support");
+    this._title_img = ImageManager.loadCharacterSelectBitmap("select_title");
+
+    this._char_sheet = ImageManager.loadCharacterSelectBitmap("select_portsmall_all");
+
+    this._big_bird_imgs = [];
+    for(let i = 1; i<=4; i++){
+        this._big_bird_imgs.push(ImageManager.loadCharacterSelectBitmap("birdport_large_"+i));
+    }
 }
 
 Scene_CharacterSelect.prototype.createDisplayObjects = function() {
@@ -131,7 +139,10 @@ Scene_CharacterSelect.prototype.createDisplayObjects = function() {
     // Setup all sprites, the order matters for layers
     this.createBackground();
     this.createTitle();
+    this.createSupports();
     this.createPortraits();
+    this.createWindowLayer();
+    this.createWindow();
 }
 
 Scene_CharacterSelect.prototype.createBackground = function() {
@@ -139,14 +150,39 @@ Scene_CharacterSelect.prototype.createBackground = function() {
     this.addChild(this._backgroundSprite);
 }
 
+Scene_CharacterSelect.prototype.createWindow = function() {
+    this.characterSelectWindow = new Window_CharacterSelect(this._portsmall_img, this._char_sheet, this._portraits);
+    this.addWindow(this.characterSelectWindow);
+}
+
+Scene_CharacterSelect.prototype.createSupports = function() {
+    this._supportSprite1 = new Sprite(this._support_img);
+    this._supportSprite1.y = Graphics.boxHeight / 2;
+    this._supportSprite1.anchor.y = 0.5;
+    this._supportSprite1.anchor.x = 1;
+    this._supportSprite1.scale.x = -1;
+    this.addChild(this._supportSprite1);
+
+    this._supportSprite2 = new Sprite(this._support_img);
+    this._supportSprite2.y = Graphics.boxHeight / 2;
+    this._supportSprite2.x = Graphics.boxWidth;
+    this._supportSprite2.anchor.y = 0.5;
+    this._supportSprite2.anchor.x = 1;
+    this.addChild(this._supportSprite2);
+}
+
 Scene_CharacterSelect.prototype.createPortraits = function() {
     this._portraits = [];
+    this._pFace = [];
     for(let i = 0; i < this.partySize; i++) {
-        let portrait = new Sprite_CharacterSelectPortrait(this._portbig_img);
+        let pFace = new Sprite(this._big_bird_imgs[0]);
+        this.addChild(pFace);
+        let portrait = new Sprite_CharacterSelectPortrait(this._portbig_img, this._big_bird_imgs, pFace);
         portrait.anchor.y = 0.5;
         portrait.y = Graphics.boxHeight / 2;
-        this.addChild(portrait);
         this._portraits.push(portrait);
+        this._pFace.push(pFace);
+        this.addChild(portrait);
     }
     // left
     this._portraits[0].x = 20;
@@ -157,6 +193,14 @@ Scene_CharacterSelect.prototype.createPortraits = function() {
     // right
     this._portraits[2].x = Graphics.boxWidth - 20;
     this._portraits[2].anchor.x = 1;
+
+    for(let i = 0; i < this.partySize; i++) {
+        this.addChild(this._portraits[i]);
+        this._pFace[i].x = this._portraits[i].x;
+        this._pFace[i].y = this._portraits[i].y;
+        this._pFace[i].anchor.y = this._portraits[i].anchor.y;
+        this._pFace[i].anchor.x = this._portraits[i].anchor.x;
+    }
 }
 
 Scene_CharacterSelect.prototype.createTitle = function() {
@@ -169,6 +213,16 @@ Scene_CharacterSelect.prototype.createTitle = function() {
 Scene_CharacterSelect.prototype.update = function() {
     Scene_Base.prototype.update.call(this);
     
+    this.updateTitleSprite();
+
+    this.timer++;
+};
+
+Scene_CharacterSelect.TitleSpeed = 15;
+Scene_CharacterSelect.prototype.updateTitleSprite = function() {
+    let w = this._titleSprite._bitmap.width;
+    let h = this._titleSprite._bitmap.height / 2;
+    this._titleSprite.setFrame(0, h * ( Math.floor(this.timer / Scene_CharacterSelect.TitleSpeed) % 2), w, h);
 };
 
 Scene_CharacterSelect.prototype.endScene = function() {
@@ -194,12 +248,296 @@ function Sprite_CharacterSelectPortrait() {
 Sprite_CharacterSelectPortrait.prototype = Object.create(Sprite_Base.prototype);
 Sprite_CharacterSelectPortrait.prototype.constructor = Sprite_CharacterSelectPortrait;
 
-Sprite_CharacterSelectPortrait.prototype.initialize = function(portrait_img) {
+Sprite_CharacterSelectPortrait.prototype.initialize = function(portrait_img, big_bird_imgs, face_sprite) {
     Sprite_Base.prototype.initialize.call(this);
+    this.big_bird_imgs = big_bird_imgs;
     this.bitmap = portrait_img;
+    this.timer = 0;
+    this.state = 0; // 0 = unselected 1 = selecting 2 = selected
+    this._faceIndex = 0;
+    this._faceFile = 0;
+
+    this.spriteFace = face_sprite;
 };
 
 Sprite_CharacterSelectPortrait.prototype.update = function() {
     Sprite_Base.prototype.update.call(this);
+    this.updateFrame();
+    this.updateContent();
+    this.timer++;
+};
+
+Sprite_CharacterSelectPortrait.AnimationSpeed = 20;
+Sprite_CharacterSelectPortrait.prototype.updateFrame = function() {
+    let w = this._bitmap.width / 2
+    let h = this._bitmap.height / 2
+    switch (this.state) {
+		case 0:
+            this.setFrame(0, h, w, h);
+            this.spriteFace.visible = false;
+            break;
+		case 1:
+            this.setFrame(w * ( Math.floor(this.timer / Sprite_CharacterSelectPortrait.AnimationSpeed) % 2) , 0, w, h);
+            this.spriteFace.visible = true;
+            break;
+		case 2:
+            this.setFrame(w, h, w, h);
+            this.spriteFace.visible = true;
+            break;	
+   };
+}
+
+Sprite_CharacterSelectPortrait.prototype.updateContent = function() {
+    let w = this.big_bird_imgs[0].width / 4;
+    let h = this.big_bird_imgs[0].height / 2;
+
+    let x = (this._faceIndex % 4) * w;
+    let y = Math.floor(this._faceIndex / 4) * h;
+
+    this.spriteFace.bitmap = this.big_bird_imgs[this._faceFile];
+    this.spriteFace.setFrame(x, y, w, h);
+}
+
+
+// ===========================================
+// * Window_CharacterSelect
+// 
+// ===========================================
+function Window_CharacterSelect() {
+    this.initialize.apply(this, arguments);
+}
+
+Window_CharacterSelect.prototype = Object.create(Window_Selectable.prototype);
+Window_CharacterSelect.prototype.constructor = Window_CharacterSelect;
+
+Window_CharacterSelect.prototype.initialize = function(frame_spritesheet, char_spritesheet, big_portraits) {
+    Window_Selectable.prototype.initialize.call(this);
+
+    this.width = Graphics.boxWidth;
+    this.height = Graphics.boxHeight;
+
+    this.frame_spritesheet = frame_spritesheet;
+    this.char_spritesheet = char_spritesheet;
+    this.big_portraits = big_portraits;
+
+    this.spriteFrames = [];
+    for(let i = 0; i < this.maxItems(); i++) {
+        let frame = new Sprite(this.frame_spritesheet);
+        frame.is_port = true;
+        this.addChild(frame);
+        this.spriteFrames.push(frame);
+    }
+    this.charSprites = [];
+    for(let i = 0; i < this.maxItems(); i++) {
+        let char = new Sprite(this.char_spritesheet);
+        char.is_port = true;
+        this.addChild(char);
+        
+        this.charSprites.push(char);
+    }
+
+    this.setHandler('ok',     this.onOk.bind(this));
+    this.setHandler('cancel', this.onCancel.bind(this));
+
+    this.partyIndex = 0; // which party member are you currently selecting
+    this.timer = 0;
+    this._pallette = 0;
+
+    // sort children to put port under the mouse
+    this.children.sort((a, b) => Number(b.is_port || 0) - Number(a.is_port || 0));
+
+    this.buildClassInfo();
+    this.setAllFrames();
+    this.start();
+};
+
+Window_CharacterSelect.prototype.buildClassInfo = function() {
+    this.classInfo = [];
+    for(let i = 0; i <= 8; i++) {  
+        this.classInfo.push([]);
+    }
+
+    $dataActors.filter(a => a && a.classId <= 8 && a.name != "").forEach(x => this.classInfo[x.classId].push(x));
+};
+
+// only support 2 for now
+Window_CharacterSelect.prototype.MaxPallettes = () => 2;
+
+Window_CharacterSelect.prototype.pallette = function() {
+    return this._pallette % this.MaxPallettes();
+}
+
+Window_CharacterSelect.prototype.onOk = function() {
+    if(this.partyIndex < 2) {
+        this.partyIndex++;
+        this.updateBigPortraits();
+        this.start();
+    } else {
+        // Start the game
+    }
+}
+
+Window_CharacterSelect.prototype.onCancel = function() {
+    if(this.partyIndex == 0) {
+        // We actually dont want to end the scene because you need to select at this point?
+        // Or maybe we do end it and go back to title idk figure this out later
+        // SceneManager._scene.endScene();
+        
+    } else {
+        this.partyIndex--;
+        // remove the current portrait
+        this.updateBigPortraits();
+        this.start();
+    }
+}
+
+Window_CharacterSelect.prototype.updateBigPortraits = function() {
+    for(let i = 0; i < 3; i++) {
+        if(this.partyIndex < i) {
+            this.big_portraits[i].state = 0;
+            continue;
+        }
+        if(this.partyIndex == i) {
+            this.big_portraits[i].state = 1;
+            continue;
+        }
+        if(this.partyIndex > i) {
+            this.big_portraits[i].state = 2;
+            continue;
+        }
+    }
+}
+
+Window_CharacterSelect.prototype.fileFromFaceName = (f) => f.charAt(f.length-1);
+
+Window_CharacterSelect.prototype.updateBigInnerPortrait = function() {
+    let actorInfo = this.classInfo[this._index+1][this.pallette()];
+    
+    this.big_portraits[this.partyIndex]._faceIndex = actorInfo.faceIndex;
+    this.big_portraits[this.partyIndex]._faceFile = this.fileFromFaceName(actorInfo.faceName);
+}
+
+Window_CharacterSelect.prototype.start = function() {
+    this.refresh();
+    this.select(0);
+    this.open();
+    this.activate();
+    this.updateBigPortraits();
+};
+
+Window_CharacterSelect.prototype.setAllFrames = function() {
+    for(let i = 0; i < this.maxItems(); i++) {
+        this.setFrameFrame(i);
+        this.setCharFrame(i);
+
+        let rect = this.itemRect(i);
+
+        this.spriteFrames[i].x = rect.x;
+        this.spriteFrames[i].y = rect.y + this.itemHeight() / 2;
+
+        this.charSprites[i].x = rect.x;
+        this.charSprites[i].y = rect.y + this.itemHeight() / 2;
+    }
+};
+
+Window_CharacterSelect.prototype.numVisibleRows = function() {
+    return 1;
+};
+
+Window_CharacterSelect.prototype.update = function() {
+    Window_Selectable.prototype.update.call(this);
+
+    this.timer++;
+    for(let i = 0; i < 8; i++) {
+        this.setFrameFrame(i);
+        this.setCharFrame(i);
+    }
+
+    this.updateBigInnerPortrait();
+};
+
+Window_CharacterSelect.prototype.maxItems = function() {
+    // This could come from somewhere else
+    return 8;
+};
+
+Window_CharacterSelect.prototype.drawItem = function(index) {
+    if(!this.spriteFrames) return;
 
 };
+
+Window_CharacterSelect.AnimationSpeed = 25;
+Window_CharacterSelect.prototype.setFrameFrame = function(index) {
+    let w = this.frame_spritesheet.width / 2;
+    let h = this.frame_spritesheet.height / 2;
+    let frame = this.spriteFrames[index];
+    if(this._index == index) {
+        let cur = (Math.floor(this.timer / Window_CharacterSelect.AnimationSpeed) % 2);
+        frame.setFrame(w * cur, 0, w, h);
+    } else {
+        frame.setFrame(0, h, w, h);
+    }
+};
+
+Window_CharacterSelect.prototype.setCharFrame = function(index) {
+    let charIndex = this.classInfo[index+1][this.pallette()].faceIndex;
+    let w = this.char_spritesheet.width / 4;
+    let h = this.char_spritesheet.height / 2;
+
+    let hIndex = Math.floor(charIndex / 4) * h;
+    let wIndex = Math.floor(charIndex % 4) * w;
+
+    this.charSprites[index].setFrame(wIndex, hIndex, w, h);
+};
+
+Window_CharacterSelect.prototype.itemWidth = function() {
+    return 90;
+};
+
+Window_CharacterSelect.prototype.itemHeight = function() {
+    return 75;
+};
+
+Window_CharacterSelect.prototype.itemRect = function(index) {
+    var rect = new Rectangle();
+    rect.width = this.itemWidth();
+    rect.height = this.itemHeight();
+
+    // We have such a wierd fucked up box
+    if(index < 4) {
+        rect.x = 40 + (rect.width + 5) * (index - 1)
+        rect.y = Graphics.boxHeight - 1.5 * rect.height;
+        // special top row
+        if(index == 0) {
+            rect.x = 5;
+            rect.y = Graphics.boxHeight - 2.5 * rect.height;
+        }
+    } else {
+        rect.x = Graphics.boxWidth - 40 - (rect.width + 5) * (4 - (index - 3));
+        rect.y = Graphics.boxHeight - 1.5 * rect.height;
+        // special top row
+        if(index == 7) {
+            rect.x = Graphics.boxWidth - rect.width - 5;
+            rect.y = Graphics.boxHeight - 2.5 * rect.height;
+        }
+    }
+    return rect;
+};
+
+Window_CharacterSelect.prototype.maxPageItems = () => 8;
+Window_CharacterSelect.prototype.maxPageRows = () => 8;
+
+Window_CharacterSelect.prototype._refreshAllParts = function() {
+    //this._refreshBack();
+    //this._refreshFrame();
+    this.drawAllItems();
+    this._refreshCursor();
+    this._refreshContents();
+    this._refreshArrows();
+    this._refreshPauseSign();
+    this._updateContents();
+};
+
+Window_CharacterSelect.prototype._refreshCursor = function() {
+    this.refresh();
+}
