30c575a45ea48da9e1cb38a9283154bae21e11e0
content-deleted <contentdeleted@gmail.com>
2022-07-25 14:43:50 -0400

    fear bar now has two states for first and second half

--- js/plugins/GH_EnemyBars.js
+++ js/plugins/GH_EnemyBars.js
@@ -24,7 +24,7 @@ function Enemy_Bar() {
         if (SceneManager._scene.constructor !== Scene_Battle) {
             return;
         }
-        let hp_bar = new Enemy_Bar(this, "hp", this._enemy.mhp, -48, true);
+        let hp_bar = new Enemy_Bar(this, "hp", this._enemy.mhp, -48, true, true);
         let tp_bar = new Enemy_Bar(this, "tp", this._enemy.maxTp(), 0, false);
         let mp_bar = new Enemy_Bar(this, "mp", this._enemy.mmp, 48, true);
 
@@ -47,7 +47,8 @@ function Enemy_Bar() {
     // max - what is the max size of the bar
     // pos - which side of the enemy to display on ie left right
     // invert - invert fill amount
-    Enemy_Bar.prototype.initialize = function(enemySprite, prop, max, x, invert) {
+    // double - load a new bar at the halfway full point
+    Enemy_Bar.prototype.initialize = function(enemySprite, prop, max, x, invert, double = false) {
         Sprite_Base.prototype.initialize.call(this);
         
         this._enemySprite = enemySprite;
@@ -57,6 +58,7 @@ function Enemy_Bar() {
         this.x = x;
         this.y = 25;
         this._invert = invert;
+        this._double = double;
         
         //this.SetPosition(this._pos);
         this.SetupSprites();
@@ -68,6 +70,10 @@ function Enemy_Bar() {
         this._bar_front_img = ImageManager.loadBarBitmap(this._prop+"_bar_front");
         this._bar_back_img = ImageManager.loadBarBitmap(this._prop+"_bar_back");
         this._numbers_img = ImageManager.loadBarBitmap("numbers");
+        if(this._double) {
+            this._bar_front_img_2 = ImageManager.loadBarBitmap(this._prop+"_bar_front_2");
+            this._bar_back_img_2 = ImageManager.loadBarBitmap(this._prop+"_bar_back_2");
+        }
 
         // create new sprites
         this._back_bar = new Sprite(this._bar_back_img);
@@ -101,6 +107,16 @@ function Enemy_Bar() {
         //this._positionSet = true;
     }
     */
+   Enemy_Bar.prototype.fillAmount = function() {
+        let cur = this._enemy["_"+this._prop];
+        if(this._double) {
+            let max = this._max / 2;
+            return this._has_swapped ? cur / max : (cur - max) / max;
+        } else {
+            let max = this._max;
+            return cur / max;
+        }
+   }
 
     Enemy_Bar.prototype.setFill = function() {
         if(!this._enemySprite.visible) {
@@ -110,7 +126,7 @@ function Enemy_Bar() {
         let w = this._front_bar._bitmap.width;
         let h = this._front_bar._bitmap.height;
         // example: _hp / _mhp
-        let fillAmount = this._enemy["_"+this._prop] / this._max;
+        let fillAmount = this.fillAmount();
         if (this._invert) fillAmount = 1 - fillAmount;
         this._front_bar.setFrame(0, GameHam.roundPixel(h * (1 - fillAmount)), w, h * fillAmount);
         this._front_bar.y = GameHam.roundPixel(h/2 * (1 - fillAmount));
@@ -143,6 +159,13 @@ function Enemy_Bar() {
         this.setFill();
         this.setText();
         // probably cache the current value and dont update if it hasnt changed
+        
+        if(this._double && !this._has_swapped && this._enemy["_"+this._prop] < this._max / 2)
+        {
+            this._back_bar.bitmap = this._bar_back_img_2;
+            this._front_bar.bitmap = this._bar_front_img_2;
+            this._has_swapped = true;
+        }
     }
 
 
